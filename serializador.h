c46c898edc7fbf04e195ca7af8bf9d573fae10b6
wajo <wajib10@gmail.com>
2019-06-18 10:33:15 -0600

    Arreglo importante de error

--- serializador.h
+++ serializador.h
@@ -4,11 +4,16 @@
 #include "rapidjson/document.h"
 #include "rapidjson/writer.h"
 #include "rapidjson/stringbuffer.h"
-
+#include "string"
+using namespace  std;
+using namespace rapidjson;
 class serializador
 {
 public:
     serializador();
+    string serializar_meta(QString nombre, QString autor, QString creacion, QString ano, QString tamano, string foto_b64);
+    void deserializarMetadata(string json, string *metadata);
+    string serializarString(string str);
 };
 
 #endif // SERIALIZADOR_H
